c8b35f5be4cd8eb5eb4ea58534a210fa8f6bed4f
Fsoky Community <61309653+Fsoky@users.noreply.github.com>
2021-09-29 20:11:56 +0600

    Update vk_bot_1.py

---
+++ source code/vk_bot_1.py
@@ -0,0 +1,23 @@
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+session = vk_api.VkApi(token="TOKEN")
+
+
+def send_message(user_id, message):
+	session.method("messages.send", 
+		{
+			"user_id": user_id,
+			"message": message,
+			"random_id": 0
+		}
+	)
+
+
+for event in VkLongPoll(session).listen():
+	if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+		text = event.text.lower()
+		user_id = event.user_id
+
+		if text == "hello":
+			send_message(user_id, "Hello!")
