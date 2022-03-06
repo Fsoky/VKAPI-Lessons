1fbe137306d872202c38924d4614e9e8967f3a87
Fsoky <cyberuest0x12@gmail.com>
2022-03-06 16:17:46 +0600

    Work with methods

--- source code/work_with_methods.py
+++ source code/work_with_methods.py
@@ -7,20 +7,20 @@ vk = session.get_api()
 
 
 def get_user_status(user_id):
-	friends = session.method("friends.get", {"user_id": user_id})
+    friends = session.method("friends.get", {"user_id": user_id})
 
-	for friend in friends["items"]:
-		user = session.method("users.get", {"user_ids": friend})
-		status = session.method("status.get", {"user_id": friend})
+    for friend in friends["items"]:
+        user = session.method("users.get", {"user_ids": friend})
+        status = session.method("status.get", {"user_id": friend})
 
-		if status["text"] == "":
-			continue
-		else:
-			print(f"{user[0]['first_name']} {user[0]['last_name']} | {status['text']}")
+        if status["text"] == "":
+            continue
+        else:
+            print(f"{user[0]['first_name']} {user[0]['last_name']} | {status['text']}")
 
 
 def set_user_status():
-	vk.status.set(text="New text for my status")
+    vk.status.set(text="New text for my status")
 
 
 get_user_status(1)
